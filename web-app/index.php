186d5138052fc57a66be3b2365c69c0040d3b7d4
varu01234 <varadhan1995nitt@gmail.com>
2016-04-24 03:16:26 +0530

    added basic ui changes

--- web-app/index.php
+++ web-app/index.php
@@ -1,25 +1,24 @@
-<html>
-<title>Band Gap Predictor</title>
-<head></head>
-<body>
-<form method="post" action="<?php echo htmlspecialchars($_SERVER["PHP_SELF"]);?>">
-<input type=text name='ele'></input>
-<input type="submit" name='but' value="submit" />
-</form>
-</body>
-</html>
+<html >
+<title > Band Gap Predictor < /title >
+<head > < / head >
+<body >
+<form method = "post" action = "<?php echo htmlspecialchars($_SERVER["PHP_SELF"]);?>" >
+<input type = text name = 'ele' > < / input >
+<input type = "submit" name = 'but' value = "submit" / >
+< / form >
+< / body >
+< / html >
 <?php
 if(isset($_POST['but']))
 {
 
-//$ele=$_POST['ele'];
-if(!empty($_POST['ele']))
-{
-$ele=$_POST['ele'];
-$command = 'python index.py '.$ele;
-exec($command, $out, $status);
-print_r($out);
-}
-else echo"<script>alert('enter the binary compound')</script>";
+    //$ele =$_POST['ele']
+    if(!empty($_POST['ele']))
+    {$ele =$_POST['ele']
+     $command = 'python index.py '.$ele
+     exec($command, $out, $status)
+     print_r($out)
+     }
+    else echo"<script>alert('enter the binary compound')</script>"
 }
 ?>
